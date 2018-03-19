23c30704a466a0b767f5a36e74ce65897686b6e0
bushitan <373514952@qq.com>
2018-03-19 18:07:48 +0800

    1.2.16

--- pages/live/script.js
+++ pages/live/script.js
@@ -6,7 +6,7 @@ module.exports = new (function () {
   var GP = null
   this.init = function (_GP){
     GP = _GP
-  },
+  }
   //更新自己的栏目
   this.sendSelf = function (e) {
     console.log(e.detail)
@@ -23,6 +23,46 @@ module.exports = new (function () {
     list.push(talk)
     GP.setData({
       feedback: list
+    })
+  }
+
+
+  this.ChangePlayer = function (index) {
+    switch (index) {
+      case 0: 
+        GP.setData({
+          showPlayerIM: true,
+          showPlayerPainter: false,
+        }); 
+        break;
+      case 1: 
+        GP.setData({
+          showPlayerIM: false,
+          showPlayerPainter: true,
+        })
+        break;
+    }
+  }
+
+  this.ChangePusher = function (index) {
+    var show = {
+      Pusher: true,
+      IM: false,
+      Player: false,
+      PusherMenu: false,
+      Cover: false,
+      Painter: false,
+      Gallery: false,
+    }
+    switch (index) {
+      case 0: show.IM = true; break;
+      case 1: show.Painter = true; break;
+      case 2: show.Gallery = true; break;
+      case 3: show.PusherMenu = true; break;
+      case 4: show.Player = true; break;
+    }
+    GP.setData({
+      show: show
     })
   }
 })
